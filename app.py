e923e269b703f5e950eed598376f1d3db08bacd0
DanielAlfonsoBabativaDiaz <daaniidiaz.16@gmail.com>
2025-09-03 00:01:47 -0500

    cambio 2

--- app.py
+++ app.py
@@ -9,9 +9,17 @@ def home():
     name = "Flask"
     return f"Hello, {name}!"
 
+@app.route('/casos_de_uso_ML')
+def casos_de_uso_ML():
+    return render_template('casos_de_uso_ML.html')
+
+@app.route('/navbar')
+def navbar():
+    return render_template('navbar.html')
+
 @app.route('/index')
 def index():
-    Myname = "Daniel"
+    Myname = "Machine_Learning"
     return render_template('index.html', name=Myname)
 
 if __name__== '__main__':
