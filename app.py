d5bb11f225e5cbb2c99679c734534251fe84fe02
DuvanArevalo <duvanlondono601@gmail.com>
2025-09-17 14:31:23 -0500

    Se realizo la prediccion de pasar curso

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from modeloCurso import evaluar_modelo
 import LinearRegression
 import joblib
+import numpy as np
+import pickle
 
 
 app = Flask (__name__)
@@ -11,6 +14,10 @@ model = joblib.load("linear_regression_model.pkl")
 @app.route('/casos_de_uso_ML')
 def casos_de_uso_ML():
     return render_template('casos_de_uso_ML.html')
+
+@app.route('/conceptos_basicos')
+def conceptos_basicos():
+    return render_template('conceptos_basicos.html')
 
 @app.route('/navbar')
 def navbar():
@@ -37,6 +44,39 @@ def calculateGrade():
         predicted_coffe_price = model.predict([[Rainfall, Temperature]])
         calculateResult = predicted_coffe_price[0]
     return render_template("prediccion.html", result = calculateResult)
+
+with open('./PKL/modelo_aprobacion.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+with open('./PKL/scaler_aprobacion.pkl', 'rb') as f:
+    scaler = pickle.load(f)
+
+@app.route("/Cursos", methods=["GET", "POST"])
+def prediccionCurso():
+    resultado = None
+
+    if request.method == "POST":
+        horas = float(request.form["horas"])
+        foros = int(request.form["foros"])
+        nivel = request.form["nivel"]
+
+        niveles = {"Secundaria": 0, "Tecnico": 1, "Universitario": 2}
+        nivel_codificado = niveles.get(nivel, 0)
+
+        entrada = np.array([[horas, foros, nivel_codificado]])
+        entrada_escalada = scaler.transform(entrada)
+        prediccion = model.predict(entrada_escalada)
+
+        resultado = "Sí" if prediccion[0] == 1 else "No"
+
+    # Métricas del modelo (siempre visibles)
+    accuracy, report_html, conf_matrix_img = evaluar_modelo(model, scaler)
+
+    return render_template("Cursos.html",
+                           result=resultado,
+                           accuracy=accuracy,
+                           report_html=report_html,
+                           conf_matrix_img=conf_matrix_img)
 
 if __name__== '__main__':
     app.run(debug=True)
